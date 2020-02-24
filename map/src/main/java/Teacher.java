dcf413270a586e9b54af084000af4e7b4d23ed0d
Ryan.Wang <ranwang1995@outlook.com>
2020-02-24 11:47:31 +0800

    [WangRan] feat: finish class Teacher

--- map/src/main/java/Teacher.java
+++ map/src/main/java/Teacher.java
@@ -1,3 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Teacher {
+
+    private String name;
+    private Map<Student, Integer> studentList;
+
+    public Teacher(String name) {
+        this.name = name;
+        studentList = new HashMap<>();
+    }
+
+    public Teacher(String name, Map<Student, Integer> studentList) {
+        this.name = name;
+        this.studentList = studentList;
+    }
+
+    public Teacher addStudent(Student newStudent) {
+        studentList.put(newStudent, newStudent.getAge());
+        return this;
+    }
+
+    public void printStudentList() {
+        for (Map.Entry<Student, Integer> student : studentList.entrySet()) {
+            System.out.println(student.getKey().toString());
+        }
+    }
 
 }
